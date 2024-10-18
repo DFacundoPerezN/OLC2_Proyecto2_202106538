0138a2d8b80d460939c4060ddd80213db9a802d7
Diego Facundo Pérez Nicolau <3607834730101@ingenieria.usac.edu.gt>
2024-10-18 00:01:25 -0600

    added For

--- compiler/cicles.js
+++ compiler/cicles.js
@@ -31,6 +31,40 @@ function translateWhile(node){
 
 }
 
+function translateFor(node){
+    console.log("Translating for");
+    let init = node.children[0];
+    let condition = node.children[1];
+    let increment = node.children[2];
+    let body = node.children[3];
+
+    var forTag = 'for_'+globalPower.tagCounter
+    globalPower.continueTag = forTag + ':'; //continue tag
+
+    var endTag = 'end_for_'+globalPower.tagCounter;
+    globalPower.breakTag = endTag + ':'; //break tag
+
+    globalPower.tagCounter++; //increment tag counter
+
+    globalPower.output += '\t#For loop\n';
+    translateSentence(init);
+    globalPower.output += forTag+': \n';
+    translateExpression(condition);
+    globalPower.output += '\t# condition: if t0 == 0 jump to '+ endTag +'\n';     
+    globalPower.output += '\tbeqz t0, '+endTag+'\t# \n\n';
+    for (const child of body.children) {
+        translateSentence(child);
+    }
+    translateExpression(increment);
+
+    globalPower.output += '\tj '+forTag+' \t #Jump to for\n';
+    globalPower.output += endTag+': \n';
+    
+    globalPower.breakTag = '';
+    globalPower.continueTag = '';
+}
+
 export {
-    translateWhile
+    translateWhile,
+    translateFor
 }
